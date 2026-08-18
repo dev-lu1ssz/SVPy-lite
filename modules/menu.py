aae5eadb8639117453b464a8c94e61afab45aa10
Luis Guilherme <guiluiscom93@gmail.com>
2026-08-18 16:32:20 -0300

    Script de gerenciamento de menu do main

---
+++ modules/menu.py
@@ -0,0 +1,8 @@
+from tabulate import tabulate
+def consulta(data):
+    if data == 'lista_consulta_clientes':
+        lista_consulta_clientes = [{'info_cliente': '', 'descrição': 'Traz todas as informações sobre os clientes cadastrados'}]
+        
+    
+if __name__ == '__main__':
+    consulta('lista_consulta_clientes')
