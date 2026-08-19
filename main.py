f8a7e66d428edcd938142c3966b04a96337715c5
Luis Guilherme <guiluiscom93@gmail.com>
2026-08-19 17:17:38 -0300

    Módulos necessários importados; Estruturação do menu de comandos para consultas de clientes

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import modules.database_creator as database_creator
 import modules.insertions as insertions
+from modules.selections import Selectdata
+import modules.menu as menu
 import os
+from tabulate import tabulate
+import sqlite3
 
-if 'SV-Py_lite_updated.db' not in os.listdir('database'):
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, 'database', 'SV-Py_lite_updated.db')
+
+if not DB_PATH:
     print('Base de dados não encontrada. Criando base de dados SV-Py')
     database_creator.main()
     
-    if 'SV-Py_lite_updated.db' not in os.listdir('database'):
+    if not DB_PATH:
         print('Ocorreu algum erro ao tentar criar a base de dados no diretório "database"')
         exit()
 
 try:
     opcao = 0
+    conexao = sqlite3.connect(DB_PATH)
+    conexao.execute('PRAGMA foreign_keys = on')
+    select = Selectdata(conexao)
+
     while opcao != 3:
         try:
             print('\nSeja bem vindo ao seu Sistema de Gerenciamento de Banco de Dados da Speed-Veiculos montado com SQLite!\n')
@@ -26,7 +38,23 @@ try:
 
             if opcao not in (1, 2, 3):
                 print('Opção inválida! Digite uma opção entre 1 e 3.')
-        
+            elif opcao == 1:
+                print('\nMostrando o menu de comandos de clientes')
+                menu.menu_clientes()
+                
+                while True:
+                    print('\nSelecione uma das opções - Digite "back" para voltar ao menu principal')
+                    comando = str(input('SVPy-lite/commands > '))
+                    lista_comandos = ['info_clinete', 'clinete_ult_os', 'cliente_pay', 'cliente_agen', 'atendimento', 'qtde_veiculos', 'back']
+                    
+                    if comando.lower() not in lista_comandos:
+                        print('\nOpção inválida! Digite um dos comandos disponíveis.')
+                    if comando.lower() == 'back':
+                        break
+                    if comando.lower() == 'info_cliente':
+                        select.client_info()
+                        
+
         except ValueError:
             print('O valor digitado é inválido!')
 
