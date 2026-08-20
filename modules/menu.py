d9e4a57439ea25d4f80fdeac54b8a7df13b076e2
Luis Guilherme <guiluiscom93@gmail.com>
2026-08-20 16:22:31 -0300

    Comando 'menu' adicionado e 'info_pagamento'

--- modules/menu.py
+++ modules/menu.py
@@ -3,10 +3,11 @@ from tabulate import tabulate
 def menu_clientes():
     lista_consulta_clientes = [('info_cliente', 'Traz todas as informações sobre os clientes cadastrados'), 
                                 ('cliente_ult_os', 'Traz informações sobre os clientes e suas ordens de serviço'),
-                                ('cliente_pay', 'Traz informações sobre clientes que realizaram pagamentos'),
+                                ('info_pagamento', 'Traz informações sobre clientes que realizaram pagamentos'),
                                 ('cliente_agen', 'Traz informações sobre clientes que solicitaram um agenciamento'),
                                 ('atendimento', 'Traz informações sobre o atendimento ao cliente'),
-                                ('qtde_veiculos', 'Traz informações sobre a quantidade de veículos que cada cliente possui')]
+                                ('qtde_veiculos', 'Traz informações sobre a quantidade de veículos que cada cliente possui'),
+                                ('menu', 'Mostra novamente o menu de comandos')]
     
     tabela = tabulate(lista_consulta_clientes, headers=['COMANDO', 'DESCRIÇÃO'], tablefmt='grid', stralign='left')
     return print(f'\n{tabela}\n')
