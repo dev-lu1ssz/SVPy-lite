4b80eca29099b614e45a8d94d35bed534c407558
Luis Guilherme <guiluiscom93@gmail.com>
2026-08-21 15:29:31 -0300

    Correção de nome

--- modules/menu.py
+++ modules/menu.py
@@ -4,7 +4,7 @@ def menu_clientes():
     lista_consulta_clientes = [('info_cliente', 'Traz todas as informações sobre os clientes cadastrados'), 
                                 ('cliente_ult_os', 'Traz informações sobre os clientes e suas ordens de serviço'),
                                 ('info_pagamento', 'Traz informações sobre clientes que realizaram pagamentos'),
-                                ('cliente_agen', 'Traz informações sobre clientes que solicitaram um agenciamento'),
+                                ('cliente_agenciamento', 'Traz informações sobre clientes que solicitaram um agenciamento'),
                                 ('atendimento', 'Traz informações sobre o atendimento ao cliente'),
                                 ('qtde_veiculos', 'Traz informações sobre a quantidade de veículos que cada cliente possui'),
                                 ('menu', 'Mostra novamente o menu de comandos'),
