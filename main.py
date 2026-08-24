32175e940c0ad800fe9fb6f7b507cf54c751fed6
Luis Guilherme <guiluiscom93@gmail.com>
2026-08-24 13:57:09 -0300

    Comando de consulta de atendimentos adicionado

--- main.py
+++ main.py
@@ -15,25 +15,25 @@ def writer(a):
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, 'database', 'SV-Py_lite_updated.db')
+color = Colors()
 
 if not DB_PATH:
-    print('Base de dados não encontrada. Criando base de dados SV-Py')
+    print(f'{color.RED}Base de dados não encontrada. Criando base de dados SV-Py{color.END}')
     database_creator.main()
     
     if not DB_PATH:
-        print('Ocorreu algum erro ao tentar criar a base de dados no diretório "database"')
+        print(f'{color.RED}Ocorreu algum erro ao tentar criar a base de dados no diretório "database"{color.END}')
         exit()
 
 try:
     opcao = 0
-    color = Colors()
     conexao = sqlite3.connect(DB_PATH)
     conexao.execute('PRAGMA foreign_keys = on')
     select = Selectdata(conexao)
 
     while opcao != 3:
         try:
-            print('\nSeja bem vindo ao seu Sistema de Gerenciamento de Banco de Dados da Speed-Veiculos montado com SQLite!\n')
+            print(f'\n{color.LIGHT_GREEN}Seja bem vindo ao seu Sistema de Gerenciamento de Banco de Dados da Speed-Veiculos montado com SQLite!{color.END}\n')
             print('Escolha uma das opções abaixo:')
             print('''
                 [1] - Ver o menu de comandos para consultas no BD
@@ -44,7 +44,7 @@ try:
             opcao = int(input(f'{color.NEGATIVE}SVPy-lite >{color.END} '))
 
             if opcao not in (1, 2, 3):
-                print('Opção inválida! Digite uma opção entre 1 e 3.')
+                print(f'{color.LIGHT_RED}\nOpção inválida! Digite uma opção entre 1 e 3.{color.END}')
             elif opcao == 1:
                 print('\nMostrando o menu de comandos de clientes')
                 menu.menu_clientes()
@@ -52,7 +52,7 @@ try:
                 while True:
                     print('\nSelecione uma das opções - Digite "back" para voltar ao menu principal')
                     comando = str(input(f'{color.NEGATIVE}SVPy-lite/commands >{color.END} '))
-                    lista_comandos = ['info_cliente', 'cliente_ult_os', 'info_pagamento', 'cliente_agenciamento', 'atendimento', 'qtde_veiculos', 'back', 'menu', 'back']
+                    lista_comandos = ['info_cliente', 'cliente_ult_os', 'info_pagamento', 'cliente_agenciamento', 'atendimento', 'qtde_veiculos', 'back', 'menu']
                     
                     if comando.lower() not in lista_comandos:
                         print(f'\n{color.RED}Opção inválida! Digite "menu" para ver a lista de comandos disponíveis.{color.END}')
@@ -141,8 +141,8 @@ try:
                         if quest_filtro.lower() in ('sim', 'ss', 's', 'yes', 'si'):
                             while True:
                                 print('\n' + tabulate([('id', 'Filtrar pelo número de registro do cliente'),
-                                                       ('status', 'Filtrar pelo status do agenciamento (Pendente/Vendido)')],
-                                                      headers=['Filtro', 'Descrição'], tablefmt='grid'))
+                                                        ('status', 'Filtrar pelo status do agenciamento (Pendente/Vendido)')],
+                                                        headers=['Filtro', 'Descrição'], tablefmt='grid'))
                                 opcao_filtro = str(input(f'\n{color.NEGATIVE}Escolha o filtro que deseja utilizar >{color.END} '))
                                 if opcao_filtro.lower() == 'id':
                                     registro = input('Digite o número de registro do cliente (ID) > ')
@@ -163,6 +163,33 @@ try:
                             writer('\nConsultando o banco de dados............\n\n')
                             select.status_agenciamento()
 
+                    elif comando.lower() == 'atendimento':
+                        quest_filtro = str(input('Deseja realizar uma consulta com filtro ? [S/N]: '))
+                        if quest_filtro.lower() in ('sim', 's', 'ss', 'yes', 'si'):
+                            while True:
+                                print('\n' + tabulate([('id_cliente', 'Filtrar pelo número de registro do cliente'),
+                                                        ('id_funcionario', 'Filtrar pelo número de registro do funcionário')], 
+                                                    headers=['Filtro', 'Descrição'], tablefmt='grid'))
+                                opcao_filtro = str(input(f'\n{color.NEGATIVE}Escolha o filtro que deseja utilizar >{color.END} '))
+                                
+                                if opcao_filtro == 'id_cliente':
+                                    registro = input('Digite o número de registro do cliente (ID) > ')
+                                    writer('\nConsultando o banco de dados............\n\n')
+                                    select.atendimento_ao_cliente(id_cliente=eval(registro))
+                                    break
+                                
+                                elif opcao_filtro == 'id_funcionario':
+                                    registro = input('Digite o número de registro do funcionário (ID) > ')
+                                    nome_funcionario, = select.funcionarios(eval(registro[0].strip()))
+                                    writer(f'{color.LIGHT_GREEN}\nConsulta: Todos atendimentos realizados pelo funcionário "{nome_funcionario}" ..........{color.END}\n\n')
+                                    select.atendimento_ao_cliente(id_funcionario=eval(registro))
+                                    break
+                                
+                                else:
+                                    print(f'{color.LIGHT_RED}Opção de filtro inválida, escolha um dos filtros disponíveis{color.END}')
+                        elif quest_filtro.lower() in ('não', 'nao', 'n', 'no'):
+                            writer('\nConsultando banco de dados............\n\n')
+                            select.atendimento_ao_cliente()
         
         except ValueError:
             print(f'{color.RED}O valor digitado é inválido!{color.END}')
